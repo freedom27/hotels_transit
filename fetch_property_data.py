d8395d743a03c7e9365fa78b89c529af1acd757c
Stefano Vettor <stefano.vettor@amadeus.com>
2020-04-09 15:23:03 +0200

    configurable cache location

--- fetch_property_data.py
+++ fetch_property_data.py
@@ -9,28 +9,29 @@ class CacheType(Enum):
     TRANSIT_LOCATION = 2
     
 class TransitCache:
-    def __init__(self):
+    def __init__(self, cache_dir):
+        self.dir = cache_dir
         self.info_cache = dict()
         self.locations_cache = dict()
         
     def load(self):
         try:
-            with open('info_cache.json', 'r') as json_file:
+            with open(self.dir + '/info_cache.json', 'r') as json_file:
                 self.info_cache = json.load(json_file)
         except:
             print('Bad Info Cache File!')
             
         try:
-            with open('locations_cache.json', 'r') as json_file:
+            with open(self.dir + '/locations_cache.json', 'r') as json_file:
                 self.locations_cache = json.load(json_file)
         except:
             print('Bad Location Cache File!')
     
     def dump(self):
-        with open('info_cache.json', 'w') as json_file:
+        with open(self.dir + '/info_cache.json', 'w') as json_file:
             json.dump(self.info_cache, json_file)
         
-        with open('locations_cache.json', 'w') as json_file:
+        with open(self.dir + '/locations_cache.json', 'w') as json_file:
             json.dump(self.locations_cache, json_file)
     
     def store(self, data, cache_type):
@@ -56,7 +57,7 @@ class TransitCache:
                 return self.locations_cache[code]
         return None
 
-cache = TransitCache()
+cache = TransitCache(config['cache']['cache_dir'])
 
 def get_data_from_cache(cache_type, property_data):
     global cache
